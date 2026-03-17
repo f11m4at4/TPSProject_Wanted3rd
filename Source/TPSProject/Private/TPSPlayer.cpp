dd3372b022db68ac06f65793ae214ff24aa23c93
jacobyi0730 <jacobyi0730@gmail.com>
2026-03-17 15:05:48 +0900

    점프

--- Source/TPSProject/Private/TPSPlayer.cpp
+++ Source/TPSProject/Private/TPSPlayer.cpp
@@ -5,7 +5,11 @@
 
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
-
+#include "InputAction.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "TPSProject.h"
 
 // Sets default values
 ATPSPlayer::ATPSPlayer()
@@ -22,9 +26,37 @@ ATPSPlayer::ATPSPlayer()
 	springArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
 	springArmComp->SetupAttachment(RootComponent);
 	springArmComp->SetRelativeLocation(FVector(0, 0, 60));
+	springArmComp->bUsePawnControlRotation = true;
+
+	bUseControllerRotationYaw = true;
 	
 	tpsCamComp = CreateDefaultSubobject<UCameraComponent>(TEXT("TpsCamComp"));
 	tpsCamComp->SetupAttachment(springArmComp);
+
+	// input action load
+	ConstructorHelpers::FObjectFinder<UInputAction> TempTurnInput(TEXT("/Script/EnhancedInput.InputAction'/Game/Input/IA_Turn.IA_Turn'"));
+	if (TempTurnInput.Succeeded())
+	{
+		ia_Turn = TempTurnInput.Object;
+	}
+
+	ConstructorHelpers::FObjectFinder<UInputAction> TempLookupInput(TEXT("/Script/EnhancedInput.InputAction'/Game/Input/IA_Lookup.IA_Lookup'"));
+	if (TempLookupInput.Succeeded())
+	{
+		ia_Lookup = TempLookupInput.Object;
+	}
+
+	ConstructorHelpers::FObjectFinder<UInputAction> TempMoveInput(TEXT("/Script/EnhancedInput.InputAction'/Game/Input/IA_Move.IA_Move'"));
+	if (TempMoveInput.Succeeded())
+	{
+		ia_Move = TempMoveInput.Object;
+	}
+	
+	ConstructorHelpers::FObjectFinder<UInputMappingContext> TempIMC(TEXT("/Script/EnhancedInput.InputAction'/Game/Input/IMC_TPS.IMC_TPS'"));
+	if (TempIMC.Succeeded())
+	{
+		imc_TPS = TempIMC.Object;
+	}
 }
 
 // Called when the game starts or when spawned
@@ -38,6 +70,36 @@ void ATPSPlayer::BeginPlay()
 void ATPSPlayer::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	// 바라보는 방향으로 이동하고 싶다.
+	// 1. 캐릭터가 바라보는 방향
+	// 2. 카메라가 바라보는 방향(컨트롤러)
+	direction = FTransform(GetControlRotation()).TransformVector(direction);
+	
+	//				v = v0 + at : 등가속운동
+	// 수직속도 계산 v = v0 + at :
+	zVelocity += gravity * DeltaTime;
+	// 수직이동 P = P0 + vt
+	FHitResult result;
+	SetActorLocation(GetActorLocation() + FVector(0, 0, zVelocity) * speed * DeltaTime, true, &result);
+
+	// 이동하고 싶다. P = P0 + vt : 등속운동
+	FVector P0 = GetActorLocation();
+	FVector vt = direction * speed * DeltaTime;
+	FVector P = P0 + vt;
+	SetActorLocation(P, true);
+
+	// zvelocity 가 언제 0이 되어야 하는가?
+	// 바닥에 있다면 ZVelocity 를 0으로 초기화
+	if (result.IsValidBlockingHit())
+	{
+		zVelocity = 0;
+		currentJumpCount = 0;
+	}
+
+	// 점프 : 사용자가 점프 버튼을 누르면 점프하고 싶다. 최대 점프 횟수만큼 점프 가능
+	
+	direction = FVector::ZeroVector;
 }
 
 // Called to bind functionality to input
@@ -45,5 +107,60 @@ void ATPSPlayer::SetupPlayerInputComponent(
 	UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// 입력 바인딩
+	auto pc = Cast<APlayerController>(Controller);
+	if (pc)
+	{
+		auto subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(pc->GetLocalPlayer());
+		if (subsystem)
+		{
+			subsystem->AddMappingContext(imc_TPS, 0);
+		}
+
+		auto playerInput = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+		if (playerInput)
+		{
+			playerInput->BindAction(ia_Turn, ETriggerEvent::Triggered, this, &ATPSPlayer::Turn);
+			playerInput->BindAction(ia_Lookup, ETriggerEvent::Triggered, this, &ATPSPlayer::Lookup);
+			//------------------------------
+			playerInput->BindAction(ia_Move, ETriggerEvent::Triggered, this, &ATPSPlayer::PlayerMove);
+			
+			playerInput->BindAction(ia_Jump, ETriggerEvent::Started, this, &ATPSPlayer::PlayerJump);
+		}
+	}
+}
+
+void ATPSPlayer::PlayerJump(const struct FInputActionValue& inputValue)
+{
+	// 최대 점프 횟수보다 작게 뛰었다면
+	if (currentJumpCount < jumpMax)
+	{
+		// 수직속도를 점프힘으로 설정한다.
+		zVelocity = jumpPower;
+		// 현재 점프 횟수를 늘려준다.
+		currentJumpCount++;
+	}
+}
+
+void ATPSPlayer::PlayerMove(const struct FInputActionValue& inputValue)
+{
+	// 상하좌우 입력을 받으면 이동하고 싶다.
+	// -> 입력을 받아서 방향을 만들자.
+	FVector2d value = inputValue.Get<FVector2d>();
+	direction.X = value.X;
+	direction.Y = value.Y;
+}
+
+void ATPSPlayer::Turn(const struct FInputActionValue& inputValue)
+{
+	float value = inputValue.Get<float>();
+	AddControllerYawInput(value);
+}
+
+void ATPSPlayer::Lookup(const struct FInputActionValue& inputValue)
+{
+	float value = inputValue.Get<float>();
+	AddControllerPitchInput(value);
 }
 
--- Source/TPSProject/Public/TPSPlayer.h
+++ Source/TPSProject/Public/TPSPlayer.h
@@ -32,5 +32,49 @@ public:
 	class USpringArmComponent* springArmComp;
 	UPROPERTY(VisibleAnywhere)	
 	class UCameraComponent* tpsCamComp;
+
+public: // -------------- 입력 -------------
+	UPROPERTY(EditDefaultsOnly, Category=Input)
+	class UInputAction* ia_Turn;
+	UPROPERTY(EditDefaultsOnly, Category=Input)
+	class UInputAction* ia_Lookup;
+	UPROPERTY(EditDefaultsOnly, Category=Input)
+	class UInputMappingContext* imc_TPS;
+
+	// 입력 처리 함수
+	// 좌우 회전 입력 처리
+	void Turn(const struct FInputActionValue& inputValue);
+	// 상하 회전 입력 처리
+	void Lookup(const struct FInputActionValue& inputValue);
+
+	UPROPERTY(EditDefaultsOnly, Category=Input)
+	class UInputAction* ia_Move;
+
+	UPROPERTY(EditAnywhere, Category=PlayerStats)
+	float speed = 600;
+
+	FVector direction;
+	
+	// 이동 입력 처리 함수
+	void PlayerMove(const struct FInputActionValue& inputValue);
+
+	// 중력가속도
+	float gravity = -10;
+	// 수직속도
+	float zVelocity = 0;
+	// 필요한 속성 : 점프 입력, 점프파워, 최대 점프 횟수, 현재 점프 횟수
+	UPROPERTY(EditDefaultsOnly, Category=Input)
+	class UInputAction* ia_Jump;
+	// 점프 입력 처리 함수
+	void PlayerJump(const struct FInputActionValue& inputValue);
+
+	UPROPERTY(EditAnywhere, Category=PlayerStats)
+	float jumpPower = 5;
+	// 최대 점프횟수
+	UPROPERTY(EditAnywhere, Category=PlayerStats)
+	int32 jumpMax = 2;
+	// 현재 점프횟수
+	int32 currentJumpCount = 0;
+
 	
 };
