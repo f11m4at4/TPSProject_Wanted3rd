9c899a09bd1030eb50f1d4c700e13ea725968671
jacobyi0730 <jacobyi0730@gmail.com>
2026-03-31 10:02:37 +0900

    PlayerBaseComponent 클래스 생성

---
+++ Source/TPSProject/Public/PlayerBaseComponent.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TPSPlayer.h"
+#include "Components/ActorComponent.h"
+#include "PlayerBaseComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TPSPROJECT_API UPlayerBaseComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UPlayerBaseComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction*
+	                           ThisTickFunction) override;
+
+public:
+	// 1. 소유액터
+	UPROPERTY()
+	ATPSPlayer* me;
+	// 2. 필요 컴포넌트
+	UPROPERTY()
+	class UCharacterMovementComponent* moveComp;
+	// 2. 입력 함수
+	virtual void SetupInputBinding(class UInputComponent* playerInput) {};
+};
